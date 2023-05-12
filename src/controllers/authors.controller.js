b43391d0e87f8770ed0e10710f8368c5207c13fd
luismavlo <lmavendano1597@gmail.com>
2023-05-11 21:11:04 -0500

    findOne and update and Create authors

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -13,7 +13,53 @@ exports.findAll = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.create = catchAsync(async (req, res, next) => {});
-exports.findOne = catchAsync(async (req, res, next) => {});
-exports.update = catchAsync(async (req, res, next) => {});
+exports.create = catchAsync(async (req, res, next) => {
+  const { name, surname, birthdate, biography, photo } = req.body;
+
+  const author = await authorsServices.create({
+    name,
+    surname,
+    birthdate,
+    biography,
+    photo,
+  });
+
+  return res.status(201).json({
+    status: 'success',
+    message: 'The author has been created 👌',
+    author,
+  });
+});
+
+exports.findOne = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+
+  const author = await authorsServices.findOne(id);
+
+  return res.status(200).json({
+    status: 'success',
+    author,
+  });
+});
+
+exports.update = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const { name, surname, birthdate, biography } = req.body;
+
+  const author = await authorsServices.findOne(id);
+
+  const authorUpdated = await authorsServices.update(author, {
+    name,
+    surname,
+    birthdate,
+    biography,
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    message: 'The author has been updated! 👌',
+    authorUpdated,
+  });
+});
+
 exports.delete = catchAsync(async (req, res, next) => {});
--- src/services/authors.service.js
+++ src/services/authors.service.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const db = require('./../database/models/index');
 
 class AuthorsServices {
@@ -9,6 +10,27 @@ class AuthorsServices {
     });
 
     return authors;
+  }
+
+  async create(authorData) {
+    return await db.Author.create(authorData);
+  }
+
+  async findOne(authorId) {
+    const author = await db.Author.findOne({
+      where: {
+        id: authorId,
+        status: true,
+      },
+    });
+
+    if (!author) throw new AppError(`User with id: ${authorId} not found`, 404);
+
+    return author;
+  }
+
+  async update(author, authorData) {
+    return await author.update(authorData);
   }
 }
 
